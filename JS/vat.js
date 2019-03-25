1ded8f9451c0575590c851eca69d31b8e994db76
Swil117 <47634996+Swil117@users.noreply.github.com>
2019-03-25 16:57:08 +0000

    Initial commit

---
+++ JS/vat.js
@@ -0,0 +1,66 @@
+
+
+
+fetch('https://api.adviceslip.com/advice')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(JSON.stringify(myJson));
+  });
+fetch('https://api.adviceslip.com/advice')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+    });
+fetch('https://api.adviceslip.com/advice')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+    });
+fetch('https://api.adviceslip.com/advice')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+    });
+fetch('https://api.adviceslip.com/advice')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+    });
+fetch('https://api.adviceslip.com/advice')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+    });
+fetch('https://api.adviceslip.com/advice')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+    });
+fetch('https://api.adviceslip.com/advice')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+    });
+fetch('https://api.adviceslip.com/advice')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+    });
